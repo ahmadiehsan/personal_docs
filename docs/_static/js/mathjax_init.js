d5b47ec15e88625c69b8ca816d61f3587f6d5c76
ehsan <1374ea@gmail.com>
2025-08-26 15:28:30 +0330

    ⬆️  update Mathjax to v4

--- docs/_static/js/mathjax_init.js
+++ docs/_static/js/mathjax_init.js
@@ -1,5 +1,21 @@
 window.MathJax = {
     chtml: {
         fontURL: "/_static/fonts/mathjax"
-    }
+    },
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        enableMenu: false,
+        menuOptions: {
+            settings: {
+                enrich: false
+            }
+        },
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    },
 };
